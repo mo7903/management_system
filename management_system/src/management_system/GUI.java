054513dca663b2ad4ced1b333efda79b8a1a1140
Mohamed_Gamil <mohammed.gamil@ejust.edu.eg>
2023-12-25 21:14:22 +0200

    Update GUI dimensions in management system

--- management_system/src/management_system/GUI.java
+++ management_system/src/management_system/GUI.java
@@ -36,7 +36,7 @@ public class GUI extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 510, Short.MAX_VALUE)
+            .addGap(0, 447, Short.MAX_VALUE)
         );
 
         pack();
