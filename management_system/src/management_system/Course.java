4c123e1bca415cc99cf6dc0841972d0c1efbb48f
Mohamed_Gamil <mohammed.gamil@ejust.edu.eg>
2023-12-26 20:31:56 +0200

    Created Student, Course classes and modeified Professor class so that it has a static ArrayList containing all professors in the university, same is done with Student. In addition both Professor and Student now interact with adding and deleting Courses through course object and not Strings.

---
+++ management_system/src/management_system/Course.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package management_system;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author moham
+ */
+public class Course {
+    private String name, id;
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private ArrayList<Professor> professors = new ArrayList<Professor>();
+    private ArrayList<User> tAs = new ArrayList<User>();
+    
+    public Course(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public ArrayList<Professor> getProfessors() {
+        return professors;
+    }
+
+
+    public ArrayList<User> gettAs() {
+        return tAs;
+    }
+
+    public void addStudent(Student s){
+       students.add(s);
+    }
+    
+    public void addProfessor(Professor s){
+       professors.add(s);
+    }
+    
+    public void addTA(User s){
+       tAs.add(s);
+    }
+    
+    public void displayCourseInfo(){
+        System.out.println(this);
+        System.out.println("Professors: ");
+        professors.forEach(e->System.out.println(e.getId() +" "+ e.getName() + " " + e.getDept()));
+        System.out.println("Students: ");
+        students.forEach(e->System.out.println(e.getId() +" "+ e.getName() + " " + e.getDept()));
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " " + this.name;
+    }
+
+    
+    
+}
--- management_system/src/management_system/Professor.java
+++ management_system/src/management_system/Professor.java
@@ -3,18 +3,22 @@ package management_system;
 import java.util.ArrayList;
 
 public class Professor extends User {
+    private static ArrayList<Professor> UniversityProfessors = new ArrayList<Professor>();
+    
     String dept;
     // Later, will be adjusted to a list of Course Objects
-    ArrayList<String> currentCourses = new ArrayList<String>();
+    ArrayList<Course> currentCourses = new ArrayList<Course>();
 
     public Professor(String name, String id, String dept) {
         super(name, id);
         this.dept = dept;
+        appendProfessor();
     }
-    public Professor(String name, String id, String dept, ArrayList<String> currentCourses) {
+    public Professor(String name, String id, String dept, ArrayList<Course> currentCourses) {
         super(name, id);
         this.dept = dept;
         this.currentCourses = currentCourses;
+        appendProfessor();
     }
 
     public String getDept() {
@@ -25,15 +29,20 @@ public class Professor extends User {
         this.dept = dept;
     }
 
-    public void addCourse(String course) {
+    public void addCourse(Course course) {
         this.currentCourses.add(course);
+        course.getProfessors().add(this);
     }
 
-    public void dropCourse(String course) {
+    public void dropCourse(Course course) {
         this.currentCourses.remove(course);
+        course.getProfessors().remove(this);
     }
 
     public void clearCourses() {
+        for(Course c:currentCourses){
+            c.getProfessors().remove(this);
+        }
         this.currentCourses.clear();
     }
 
@@ -50,16 +59,29 @@ public class Professor extends User {
                 ", current_courses = " + currentCourses +
                 '}';
     }
+    
+    private void appendProfessor(){
+        UniversityProfessors.add(this);
+    }
+    
+    public static void removeProfessor(Professor p){
+        UniversityProfessors.remove(p);
+    }
 // Mini-Test
-/*
+    
     public static void main(String[] args) {
         Professor fares = new Professor("Ahmed Fares", "1", "CSE");
         System.out.println(fares);
-        fares.addCourse("Computer Organization");
-        fares.addCourse("Intro to Programming");
+        Course CO = new Course("Computer Organization", "CSE321");
+        Course ItoP = new Course("Intro to Programming", "CSE211");
+        fares.addCourse(CO);
+        fares.addCourse(ItoP);
         fares.displayCourses();
-        fares.dropCourse("CO2");
+        System.out.println(CO);
+        fares.dropCourse(CO);
+        System.out.println(fares);
         fares.displayCourses();
+        ItoP.displayCourseInfo();
     }
-*/
+
 }
